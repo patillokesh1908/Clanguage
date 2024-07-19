8b8e92bea240fa05a06979ceb0a99b963c413ac5
patil lokesh <patilluckyrr@gmail.com>
2024-07-19 18:46:31 +0530

    Add files via upload

---
+++ lokesh.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+  void main  ( )
+   { printf ("Name:patil lokesh\n");
+     printf ("school: sunrise\n");
+     printf ("age:17\n");
+     
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+
+   }
