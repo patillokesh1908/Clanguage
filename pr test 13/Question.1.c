22a4627584ca264f0033ca6f4df26aabfccc75d9
patil lokesh <patilluckyrr@gmail.com>
2024-09-13 18:58:11 +0530

    Add files via upload

---
+++ pr test 13/Question.1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+  void main()
+  
+{
+	int a,b;
+	printf("enter a:");
+	scanf("%d",&a);
+	 
+	printf("enter b:");
+	scanf("%d",&b);
+	
+	{
+		if (a<b)
+		printf("%d",a);	
+		
+	}
+    
+	{
+		printf("%d",b);
+	}
+	
+  }  
+  
+  
+  
---
+++ pr test 13/Question2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+ void main()
+ 
+  int a,b,c;
+   	
+{
+	printf("enter a:");
+	scanf("%d",&a); 
+	
+	printf("enter b:");
+	scanf("%d",&b);
+		
+	printf("enter c:");
+	scanf("%d",&c);
+	
+	if (a>b)
+	{
+		print("%d",a);	
+	}
+    else
+    {
+    	printf("%d",b);
+	}
+	else
+	{
+		printf("%d",c);
+	}
+	else
+}
