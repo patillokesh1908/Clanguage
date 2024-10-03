1317c483e791bf7454db7f0aa9b87992db09d16e
patil lokesh <patilluckyrr@gmail.com>
2024-10-03 06:04:13 -0700

    Add files via upload

---
+++ PATTERN CRI/5.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,k,l,m;
+	
+	for(i=5; i>=1; i--)
+	{
+		for(k=i; k>=2; k--)
+		{
+			printf("  ");
+		}
+		
+		
+		for(j=i; j<=5; j++)
+		{
+			printf("%d ",j);
+		}
+		
+		for(l=4; l>=i; l--)
+		{
+		    printf("%d ",l);
+		}
+		printf("\n");
+		
+		
+	
+	}
+}
