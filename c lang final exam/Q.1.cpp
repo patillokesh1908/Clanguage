933610a2d2714a249f9de75cacd7a72b036b4765
patil lokesh <patilluckyrr@gmail.com>
2024-12-11 17:22:08 +0530

    Add files via upload

--- c lang final exam/Q.1.cpp
+++ c lang final exam/Q.1.cpp
@@ -1,19 +1,74 @@
 #include<stdio.h>
 #include<conio.h>
 
-  void main()
+   main()
   
   // write a program in to check whether a number is even or odd using a user defined function
  
 {
-	 int a,b;
-	 printf("enter num odd");
-	 scanf("%d,&first");
-	 
-	 printf("enter the even");
-	 scanf("%d,&first");
-	 
-	 
-	 
-	 
+
+		int a,b,c,d;
+		printf("enter a : ");
+		scanf("%d",&a);
+		printf("enter b : ");
+		scanf("%d",&b);
+		printf("enter c : ");
+		scanf("%d",&c);
+		printf("enter d : ");
+		scanf("%d",&d);
+
+
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("a is odd");
+			}
+			else
+			{
+				printf("d is odd");	
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is even");	
+			}
+			else
+			{
+				printf("d is even ");	
+			}	
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				printf("b is odd ");
+			}
+			else
+			{
+				printf("d is odd ");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is even");
+			}
+			else
+			{
+				printf("d is even");
+			}
+		}
+	
+	}
+	
 }
+ 	
--- c lang final exam/Q.2.cpp
+++ c lang final exam/Q.2.cpp
@@ -1,15 +1,41 @@
 #include<stdio.h>
 #include<stdio.h>
 
-  void main()
+   main()
   
-  
+  // Write Below Pattern Using Nested Loop
+      
+        //45 46 47 48 49
+
+        //46 47 48 49
+
+        //47 48 49
+
+        //48 49
+
+        //49                               
+
 {
+	int i,j,k;
+	
+
+		int: i,j,k;
+		printf("enter  : ");
+		scanf("%d",&i);
+		printf("enter i : ");
+		scanf("%d",&j);
+		printf("enter j : ");
+		scanf("%d",&c);
+		printf("enter k : ");
+		scanf("%d",&k ;
+
+
+ 	
 	
 	
 	
 	
-}
+
   	
   
   
